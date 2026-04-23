27c57050b02c43e413a60e5e1e06ef3af267786d
퐝코니 <91484435+pwangcony@users.noreply.github.com>
2026-04-23 11:20:49 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
+  base: '/SAM4STEST01/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
